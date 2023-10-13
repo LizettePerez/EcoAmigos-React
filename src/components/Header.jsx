d445fb971104560808951acb349f5333472b60a8
LizettePerez <lizette.perezcorrea@gmail.com>
2023-10-13 10:20:09 -0300

    agregar Link to en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import Logo from '../assets/logo.png';
 import { Fragment, useState } from 'react';
 import { Dialog, Popover, Transition } from '@headlessui/react';
+import { Link } from 'react-router-dom';
+
 
 import {
   Bars3Icon,
@@ -47,37 +49,37 @@ export default function Example() {
             </Transition>
           </Popover>
 
-          <a href="/#inicio" className="text-sm leading-6  custom-active">
+          <Link to="/#inicio" className="text-sm leading-6  custom-active">
             Inicio
-          </a>
-          <a href="/#reciclaje" className="text-sm leading-6 custom-a">
+          </Link>
+          <Link to="/#reciclaje" className="text-sm leading-6 custom-a">
             Reciclaje
-          </a>
-          <a href="/#eventos" className="text-sm leading-6 custom-a">
+          </Link>
+          <Link to="/#eventos" className="text-sm leading-6 custom-a">
             Eventos
-          </a>
-          <a href="/#contacto" className="text-sm leading-6 custom-a">
+          </Link>
+          <Link to="/#contacto" className="text-sm leading-6 custom-a">
             Contacto
-          </a>
+          </Link>
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="/comunidad" className="text-sm leading-6 custom-a">
+          <Link to="/comunidad" className="text-sm leading-6 custom-a">
             Iniciar Sesión <span aria-hidden="true">&rarr;</span>
-          </a>
+          </Link>
         </div>
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="/#inicio" className="-m-1.5 p-1.5">
+            <Link to="/#inicio" className="-m-1.5 p-1.5">
               <span className="sr-only">Open user menu</span>
               <img
                 className="h-8 w-auto"
                 src={Logo}
                 alt="logo"
               />
-            </a>
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -90,38 +92,33 @@ export default function Example() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <a
-                  href="/#inicio"
+                <Link to="/#inicio"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-sm leading-6 custom-a custom-active"
                 >
                   Inicio
-                </a>
-                <a
-                  href="/#reciclaje"
+                </Link>
+                <Link to="/#reciclaje"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-sm leading-6 custom-a"
                 >
                   Reciclaje
-                </a>
-                <a
-                  href="/#eventos"
+                </Link>
+                <Link to="/#eventos"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-sm leading-6 custom-a"
                 >
                   Eventos
-                </a>
-                <a
-                  href="/#contacto"
+                </Link>
+                <Link to="/#contacto"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-sm leading-6 custom-a"
                 >
                   Contacto
-                </a>
+                </Link>
               </div>
               <div className="py-6">
-                <a
-                  href="/comunidad"
+                <Link to="/comunidad"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-sm leading-6 custom-a"
                 >
                   Iniciar Sesión
-                </a>
+                </Link>
               </div>
             </div>
           </div>
