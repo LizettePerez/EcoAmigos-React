32b3a59220dd8d92f1d8858ce9dab2a04137d459
lujoperez <lujo.perez21@gmail.com>
2023-10-19 17:25:32 -0300

    conexion API final

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
 export default function Text() {
-  const [postText, setPostText] = useState('');
+  const [postText, setPostText] = useState("");
 
   const handlePostTextChange = (e) => {
     setPostText(e.target.value);
-  }
+  };
 
   const handlePostSubmit = () => {
-    if (postText.trim() === '') {
+    if (postText.trim() === "") {
       console.log("El texto del post está vacío. No se enviará la solicitud.");
       return;
     }
@@ -17,27 +17,29 @@ export default function Text() {
     const userEmail = localStorage.getItem("email");
     console.log("Email del usuario:", userEmail);
 
-    axios.post('http://localhost:8080/post/guardar', {
-      postTexto: postText,
-      usuario: localStorage.getItem("email")
-    })
+    axios
+      .post("http://localhost:8080/post/guardar", {
+        postTexto: postText,
+        email: localStorage.getItem("email"),
+      })
       .then((response) => {
         console.log("Post del usuario enviados con éxito: ", response.data);
       })
       .catch((error) => {
         console.log("Error al enviar datos: ", error);
       });
-  }
-
+  };
 
   return (
-    <div className='post'>
+    <div className="post">
       <textarea
         placeholder="Escribe tu post aquí"
         value={postText}
         onChange={handlePostTextChange}
       />
-      <button onClick={handlePostSubmit} className="communityBtn">Publicar</button>
+      <button onClick={handlePostSubmit} className="communityBtn">
+        Publicar
+      </button>
     </div>
   );
 }
